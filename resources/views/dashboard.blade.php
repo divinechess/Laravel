6856560a3313a61d86a46e678f2bcc3a6fb2db0b
John Allott <johnallott27@hotmail.com>
2021-05-28 19:04:18 +0100

    Added bootstrap and JS and table

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,39 @@
 <x-app-layout>
     <x-slot name="header">
+
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
+
+
         </h2>
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
-            </div>
-        </div>
+       <div class="container">
+           <div class="row">
+               <table class="table">
+                   <thead>
+                   <tr>
+                       <th scope="col">S No</th>
+                       <th scope="col">Name</th>
+                       <th scope="col">Email</th>
+                       <th scope="col">Created at</th>
+                   </tr>
+                   </thead>
+                   <tbody>
+                        @foreach($users as $row)
+                   <tr>
+                       <th scope="row">1</th>
+                       <td>{{$row->name}}</td>
+                       <td>{{$row->email}}</td>
+                       <td>{{$row->created_at}}</td>
+                   </tr>
+                        @endforeach
+
+                   </tbody>
+               </table>
+           </div>
+
+       </div>
     </div>
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\ContactController;
+use App\Models\User;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -29,5 +30,7 @@ Route::get('/about', function () {
 Route::get('/contact blah blah blah zzz',[ContactController::class,'index'])->name('con');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
+
+    $users = User::all();
+    return view('dashboard',compact('users'));
 })->name('dashboard');
